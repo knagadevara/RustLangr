25b5953fc65bca2ff3909c3b4cedabbd85e97fe5
knagadevara <vnsk.1991@gmail.com>
2024-01-31 00:48:47 +0530

    rustish2

---
+++ level_1/src/_01_hello_world.rs
@@ -0,0 +1,6 @@
+pub fn hello_world(){
+  println!("Hello, world!");
+  println!("O1");
+  println!("O2");
+  println!("O3")   
+}
---
+++ level_1/src/main.rs
@@ -0,0 +1,9 @@
+mod _01_hello_world;
+mod _02_integers;
+mod _03_float;
+
+fn main() {
+ _01_hello_world::hello_world();
+ _02_integers::demo_integers();
+ _03_float::demo_float();
+}
